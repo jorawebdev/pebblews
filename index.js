ca62286fde299d63d8832a72196c1bf12a648d6e
jorawebdev <jorawebdev@gmail.com>
2016-03-16 15:39:58 -0700

    added string

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+//var port = process.env.PORT || 5000
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
-server.listen(port)
+//server.listen(port)
 
-console.log("http server listening on %d", port)
+//console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created");
