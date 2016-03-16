b601dd5d4ce62499bb2533474214a04dac92270c
jorawebdev <jorawebdev@gmail.com>
2016-03-16 15:45:51 -0700

    added string

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 //server.listen(port)
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 //console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
